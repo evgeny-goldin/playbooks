54cdc984cadd339605e8e1bd08d4387046e3f303
Evgeny Goldin <evgenyg@gmail.com>
2014-09-16 00:56:53 +0200

    All playbooks are run by Shippable

--- shippable.sh
+++ shippable.sh
@@ -3,4 +3,8 @@
 set -x
 
 ./packer/ansible/install-ansible.sh
-ansible-playbook playbooks/packer-ubuntu.yml -c local
+
+ansible-playbook playbooks/asgard-ubuntu.yml  -c local
+ansible-playbook playbooks/jenkins-ubuntu.yml -c local
+ansible-playbook playbooks/mysql-ubuntu.yml   -c local
+ansible-playbook playbooks/packer-ubuntu.yml  -c local
