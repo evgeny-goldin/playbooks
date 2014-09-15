a48cdd74f74bf97f23ea662c200c7e483df3c569
Evgeny Goldin <evgenyg@gmail.com>
2014-09-15 23:28:32 +0200

    Ansible install update

--- packer/ansible/install-ansible.sh
+++ packer/ansible/install-ansible.sh
@@ -6,7 +6,7 @@ sudo apt-get update  -y
 sudo apt-get install -y vim htop wget curl git unzip python-pip python-dev
 sudo pip install ansible==1.7.1
 
-mkdir -p         /etc/ansible
+sudo mkdir -p    /etc/ansible
 echo localhost > /etc/ansible/hosts
 
 ansible          --version
