5db9cf74011f9b4d6308f700a0d5bdb2ac18d01c
Evgeny Goldin <evgeny.goldin@akqa.com>
2014-09-16 16:31:06 +0200

    shippable.sh update

--- shippable.sh
+++ shippable.sh
@@ -9,9 +9,9 @@ set -x
 # Ansible
 # ---------------
 
-ansible-playbook playbooks/asgard-ubuntu.yml  -c local -e "asgard_start_service=false AWS_ACCOUNT_ID=N AWS_ACCESS_KEY_ID=N AWS_SECRET_ACCESS_KEY=N"
-ansible-playbook playbooks/jenkins-ubuntu.yml -c local -e "jenkins_start_service=false"
-# ansible-playbook playbooks/mysql-ubuntu.yml   -c local -e "mysql_start_service=false"
+ansible-playbook playbooks/asgard-ubuntu.yml  -c local -e "periodic=true asgard_start_service=false AWS_ACCOUNT_ID=N AWS_ACCESS_KEY_ID=N AWS_SECRET_ACCESS_KEY=N"
+ansible-playbook playbooks/jenkins-ubuntu.yml -c local -e "periodic=true jenkins_start_service=false"
+# ansible-playbook playbooks/mysql-ubuntu.yml   -c local -e "periodic=true mysql_start_service=false"
 ansible-playbook playbooks/packer-ubuntu.yml  -c local
 
 # ---------------
@@ -19,7 +19,7 @@ ansible-playbook playbooks/packer-ubuntu.yml  -c local
 # ---------------
 
 for box in ansible:1.7.1 jenkins packer; do
-  local name=$(echo $box | sed -r 's/:.+//')
+  name=$(echo $box | sed -r 's/:.+//')
   cp "docker/$name/Dockerfile" .
   docker build --rm --no-cache -t "evgenyg/$box" .
 done
