17d934b8ab247b28953b175bbdf306d42ef9158e
Evgeny Goldin <evgenyg@gmail.com>
2014-09-16 21:51:13 +0200

    shippable.sh updates

--- shippable.sh
+++ shippable.sh
@@ -4,21 +4,21 @@ set -e
 set -x
 
 ./docker/ansible/install-ansible.sh
+ansible-playbook playbooks/packer-ubuntu.yml -c local
 
 # ---------------
 # Ansible
 # ---------------
 
-ansible-playbook playbooks/asgard-ubuntu.yml  -c local -e "periodic=true asgard_start_service=false AWS_ACCOUNT_ID=N AWS_ACCESS_KEY_ID=N AWS_SECRET_ACCESS_KEY=N"
-ansible-playbook playbooks/jenkins-ubuntu.yml -c local -e "periodic=true jenkins_start_service=false"
-ansible-playbook playbooks/mysql-ubuntu.yml   -c local -e "periodic=true mysql_start_service=false"
-ansible-playbook playbooks/packer-ubuntu.yml  -c local
+# ansible-playbook playbooks/asgard-ubuntu.yml  -c local -e "periodic=true asgard_start_service=false AWS_ACCOUNT_ID=N AWS_ACCESS_KEY_ID=N AWS_SECRET_ACCESS_KEY=N"
+# ansible-playbook playbooks/jenkins-ubuntu.yml -c local -e "periodic=true jenkins_start_service=false"
+# ansible-playbook playbooks/mysql-ubuntu.yml   -c local -e "periodic=true mysql_start_service=false"
 
 # ---------------
 # Docker
 # ---------------
 
-for box in ansible:1.7.1 jenkins packer; do
+for box in ansible:1.7.1; do
   name=$(echo $box | sed -r 's/:.+//')
   cp "docker/$name/Dockerfile" .
   docker build --rm --no-cache -t "evgenyg/$box" .
