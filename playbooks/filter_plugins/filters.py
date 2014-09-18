0555e33ec4725529da7117a3f2a08dc690a1a606
Evgeny Goldin <evgeny.goldin@akqa.com>
2014-09-18 13:08:00 +0200

    Ruby role added

--- playbooks/filter_plugins/filters.py
+++ playbooks/filter_plugins/filters.py
@@ -1,4 +1,5 @@
 import time;
+import subprocess;
 
 def strftime( epoch_time ):
   '''time.strftime wrapper'''
@@ -13,12 +14,20 @@ def explain( seconds ):
   seconds = ( seconds - ( hours * 3600 ) - ( minutes * 60 ))
   return "{0:02d}:{1:02d}:{2:02d}".format( hours, minutes, seconds )
 
+def calculate( version, latest_version_command ):
+  '''Retrieves latest version if needed by running the command specified'''
+  if version == 'latest':
+    return subprocess.check_output( latest_version_command, shell=True ).strip()
+  else:
+    return version
+
 
 class FilterModule( object ):
   ''' Custom Ansible filters '''
 
   def filters( self ):
     return {
-      'strftime': strftime,
-      'explain' : explain,
+      'strftime'  : strftime,
+      'explain'   : explain,
+      'calculate' : calculate,
     }
