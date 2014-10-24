99e404cd83fb054e376db5ad5e582cab1756778c
Evgeny Goldin <evgenyg@gmail.com>
2014-10-24 18:49:35 +0200

    Ansible Docker - 'htop' package added

--- docker/ansible/install-ansible.sh
+++ docker/ansible/install-ansible.sh
@@ -5,7 +5,7 @@ set -x
 sed -i 's/archive\.ubuntu\.com/ubuntu.mirror.su.se/' /etc/apt/sources.list
 sudo apt-get update  -y
 sudo apt-get upgrade -y
-sudo apt-get install -y vim git man wget curl build-essential psmisc
+sudo apt-get install -y vim git man wget curl htop build-essential psmisc
 sudo apt-get install -y python-pip python-dev
 sudo pip install --upgrade ansible
 
