dfd11b587f88eff1f3d931471b0136f9841b994a
Evgeny Goldin <evgenyg@gmail.com>
2014-10-24 17:14:40 +0200

    Ruby Docker - PATH added, only 'bundler' gem installed

--- playbooks/roles/ruby/templates/install-ruby.sh
+++ playbooks/roles/ruby/templates/install-ruby.sh
@@ -5,16 +5,10 @@ source '{{ ruby.rvm.home }}/scripts/rvm'
 set -x
 
 rvm get stable
-rvm --version
 rvm install        '{{ ruby_version }}'
 rvm  --default use '{{ ruby_version }}'
 gem  --version
 rake --version
-
 gem update --system
-
-{% for gem in ruby.gems %}
-gem install '{{ gem }}'
-{% endfor %}
-
+gem install bundler
 gem update
