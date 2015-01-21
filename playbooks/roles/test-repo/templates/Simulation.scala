ac171eebaca1f087054e09b4205126436db00da9
Evgeny Goldin <evgenyg@gmail.com>
2015-01-21 18:18:16 +0100

    test-repo - running simulation is paramettrized and extracted

--- playbooks/roles/test-repo/templates/Simulation.scala
+++ playbooks/roles/test-repo/templates/Simulation.scala
@@ -3,20 +3,18 @@ import io.gatling.core.structure.ScenarioBuilder
 import scala.io.Source
 import io.gatling.http.Predef._
 
-class LoadArtifacts extends Simulation {
+class {{ name }} extends Simulation {
 
-  val baseURL   = "{{ repo }}"
-  val artifacts = "{{ test_repo.load.artifacts }}"
-  val scn       = Source.fromFile( artifacts ).getLines().foldLeft( scenario( "LoadArtifacts" )) {
+  val scn = Source.fromFile( "{{ artifacts }}" ).getLines().foldLeft( scenario( "{{ name }}" )) {
     ( scn, artifact ) => scn.exec( http( artifact.split( '/' ).last ).get( artifact ))
   }
 
-  val httpConf = http.baseURL( baseURL ).
+  val httpConf = http.baseURL( "{{ repo }}" ).
                       acceptHeader( "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" ).
                       doNotTrackHeader( "1" ).
                       acceptLanguageHeader( "en-US,en;q=0.5" ).
                       acceptEncodingHeader( "gzip, deflate" ).
                       userAgentHeader( "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:35.0) Gecko/20100101 Firefox/35.0" )
 
-  setUp( scn.inject( atOnceUsers( {{ test_repo.load.users }} ))).protocols( httpConf )
+  setUp( scn.inject( atOnceUsers( {{ users }} ))).protocols( httpConf )
 }
