da95c247bf783ef316882fd7e56c6421bf0151c9
Evgeny Goldin <evgenyg@gmail.com>
2015-02-15 00:18:36 +0100

    Test repo role - Simulation.scala refactoring

--- playbooks/roles/test-repo/templates/Simulation.scala
+++ playbooks/roles/test-repo/templates/Simulation.scala
@@ -1,47 +1,57 @@
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
 import io.gatling.core.structure.ScenarioBuilder
 import scala.io.Source
 import io.gatling.http.Predef._
 
 class {{ repo_name }}{{ item.name }} extends Simulation {
 
-  val artifacts = Source.fromFile( "{{ test_repo.files_dir }}/{{ item.artifacts }}" ).getLines()
+  // Artifacts read from the file, each one as Array[String], split by "/"
+  // Lines having less than 4 elements (groupId, artifactId, version, artifact) are ignored
+  val artifacts:Iterator[Array[String]] = Source.fromFile( "{{ test_repo.files_dir }}/{{ item.artifacts }}" ).
+                                          getLines().map( _.split( "/" )).filter( _.size > 3 )
+  // Coordinates extractors
+  def groupId   ( artifact: Array[String] ) = artifact.take( artifact.size - 3 ).mkString( "." )
+  def artifactId( artifact: Array[String] ) = artifact.takeRight( 3 )( 0 )
+  def version   ( artifact: Array[String] ) = artifact.takeRight( 2 )( 0 )
 
-  def buildChain( coordinateName: String, path: String, extractor: String => String ) =
-    artifacts.map( extractor ).toList.distinct.sorted.foldLeft( exec()){
-    ( e, coordinate ) =>
-    val query = path.replace( s"<${coordinateName}>", coordinate )
-    e.exec( http( query ).get( query ))
-  }
+  /**
+   * Builds a chain of calls based on query path,
+   * token to replace in path and extractor returning the value to replace the token with given the artifact
+   */
+  def buildChain( path: String, token: String, extractor: Array[String] => String ):ChainBuilder =
+    buildChain( path, List(( token, extractor )))
 
-  def groupId   ( artifact:String ) = { val array = artifact.split( "/" )
-                                        array.take( array.size - 3 ).mkString( "." )}
-  def artifactId( artifact:String ) = artifact.split( "/" ).takeRight( 3 )( 0 )
-  def version   ( artifact:String ) = artifact.split( "/" ).takeRight( 2 )( 0 )
+  /**
+   * Builds a chain of calls based on query path and
+   * pairs of (token to replace in path, extractor returning the value to replace the token with given the artifact)
+   */
+  def buildChain( path: String, tokens: Iterable[( String, Array[String] => String )] ):ChainBuilder =
+    // artifacts => queries => sorted and unique queries => execs
+    artifacts.map {
+      artifact: Array[String] => tokens.foldLeft( path ) {
+        ( query, tokenAndExtractor ) => query.replace( s"<${tokenAndExtractor._1}>", tokenAndExtractor._2( artifact ))
+      }
+    }.
+    toList.distinct.sorted.
+    foldLeft( exec()){
+      ( e, query ) => e.exec( http( query ).get( query ))
+    }
+
 
   {% if   ( item.search | default('')) == 'quick' %}
-  val chain = buildChain( "name", "{{ quick_search }}", artifactId( _ ).split( '-' )( 0 ))
+  val chain = buildChain( "{{ quick_search }}", "name", artifactId( _ ).split( '-' )( 0 ))
   {% elif ( item.search | default('')) == 'groupId' %}
-  val chain = buildChain( "g", "{{ groupId_search }}", groupId )
+  val chain = buildChain( "{{ groupId_search }}", "g", groupId )
   {% elif ( item.search | default('')) == 'artifactId' %}
-  val chain = buildChain( "a", "{{ artifactId_search }}", artifactId )
+  val chain = buildChain( "{{ artifactId_search }}", "a", artifactId )
   {% elif ( item.search | default('')) == 'version' %}
-  val chain = buildChain( "v", "{{ version_search }}", version )
+  val chain = buildChain( "{{ version_search }}", "v", version )
   {% elif ( item.search | default('')) == 'gav' %}
-  val chain = artifacts.foldLeft( exec()){
-    ( e, artifact ) =>
-    val query = "{{ gav_search }}".replace( "<g>", groupId( artifact )).
-                                   replace( "<a>", artifactId( artifact )).
-                                   replace( "<v>", version( artifact ))
-    e.exec( http( query ).get( query ))
-  }
+  val chain = buildChain( "{{ gav_search }}", List(( "g", groupId ), ( "a", artifactId ), ( "v", version )))
   {% else %}
-  val chain = artifacts.foldLeft( exec()){
-    ( e, artifact ) =>
-    val query = "{{ repo }}".replace( "<repo>",     "{{ import_repo }}" ).
-                             replace( "<artifact>", artifact )
-    e.exec( http( query ).get( query ))
-  }
+  val chain = buildChain( "{{ repo }}", List(( "repo",     artifact => "{{ import_repo }}" ),
+                                             ( "artifact", artifact => artifact.mkString( "/" ))))
   {% endif %}
 
   // http://gatling.io/docs/2.1.4/general/scenario.html#scenario-loops
