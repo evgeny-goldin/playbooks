eac7c5ed672d80fae074c0e28e20efdae2d684ed
Evgeny Goldin <evgenyg@gmail.com>
2015-03-02 10:41:00 +0100

    docker-build.sh updates

--- docker-build.sh
+++ docker-build.sh
@@ -18,11 +18,17 @@ set -e
 set -x
 
 cp "$dockerfile" .
-time docker build --rm --no-cache -t="$owner/$image" .
-rm  Dockerfile
-
-image_id="$(docker images "$owner/$image" | grep latest | awk '{print $3}')"
-docker tag "$image_id" "$owner/$image:$tag"
+# http://stackoverflow.com/a/12451419/472153
+exec 5>&1
+output=$(docker build --rm --no-cache -t="$owner/$image" . | tee >(cat - >&5))
+image_id=$(echo "$output" | tail -1 | awk '{print $3}')
+docker tag -f "$image_id" "$owner/$image:$tag"
 
 set +e
 set +x
+
+echo '-------------------------------------------------------------------------'
+echo "Image '$image_id' is built from '$dockerfile' and tagged as '$owner/$image:$tag' "
+echo '-------------------------------------------------------------------------'
+
+rm  Dockerfile
